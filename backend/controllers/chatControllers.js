c90a88de54deeec1f665a7d51152d9464acb8cfc
“[WEb <gouravsingla830@gmail.com>
2024-04-22 01:10:30 +0530

    setting up the chats controllers

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,7 +1,59 @@
 const expressAsyncHandler = require("express-async-handler");
 
-const accessChat = expressAsyncHandler(async () => {
+const Chat = require("../models/chatModals");
+const User = require("../models/User");
+const { registerUser } = require("./userControllers");
+const accessChat = expressAsyncHandler(async (req,res) => {
   const { userId } = req.body;
   if (!userId) {
+    console.log("UserId params no req sent");
+    return res.sendStatus(400);
+  }
+  let isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      {
+        users: { $elemMatch: { $eq: req.user._id } },
+      },
+      {
+        users: { $elemMatch: { $eq: userId } },
+      },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    let ChatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+    try {
+      const createdChat = await Chat.create(ChatData);
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).send(fullChat);
+    } catch (err) {
+      res.status(400);
+      throw new error(error.message);
+    }
   }
 });
+
+const fetchChats = expressAsyncHandler(async (req,res) => {
+  try{
+    Chat.find({users:{$elemMatch :{$eq:req.user._id}}}).then((result)=>res.send(result))
+  }catch(err){
+
+  }
+});
+
+module.exports = accessChat,fetchChats;
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const protect = require("../middleware/authMiddleWare");
+const accessChat = require("../controllers/chatControllers");
 const router = express.Router();
 router.route("").post(protect, accessChat).get(protect, fetchChat);
 router.route("/group").get(protect, createGroupChat);
