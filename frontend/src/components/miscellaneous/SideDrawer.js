beba5635357fa079ed27babb17569850ce878c04
“[WEb <gouravsingla830@gmail.com>
2024-05-18 20:33:26 +0530

    fixes

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,5 +1,13 @@
-import { Box, Button, Tooltip, Text, Menu, MenuButton, MenuList } from "@chakra-ui/react";
-import { BellIcon,ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Tooltip,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 
@@ -10,30 +18,30 @@ function SideDrawer() {
   const [loadingChat, setLoadingChat] = useState();
   return (
     <>
-      <Box
-        d="flex"
+      <Box        d="flex"
         justifyContent="space-between"
         alignItems="center"
         bg="white"
         w="100%"
         p="5px 10px 5px 10px"
-        borderWidth="5px"
-      >
+        borderWidth="5px">
         <Tooltip label="Search user to chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
             <i className="fas fa-search"></i>
-            <Text d={{ base: "none", md: "flex" }} px="4">
+            <Text d={{ base: "none", md: "flex" }} px={4}>
               Search User
             </Text>
           </Button>
         </Tooltip>
-        <Text fontFamily="2x1" fontFamily="Work sans">
+        <Text fontSize="2xl" fontFamily="Work sans">
           ChatApp
         </Text>
         <div>
           <Menu>
-            <MenuButton p={1}><BellIcon fontSize="2x1" m={1} /></MenuButton>
-{/* <MenuList></MenuList> */}
+            <MenuButton p={1}>
+            <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
           </Menu>
         </div>
       </Box>
