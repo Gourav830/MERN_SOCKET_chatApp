ad760e6bc4a12287e766a995e3a7a6f05f3f95d3
“[WEb <gouravsingla830@gmail.com>
2024-05-21 21:57:12 +0530

    little changes

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -6,25 +6,31 @@ import {
   Menu,
   MenuButton,
   MenuList,
+  MenuItem,
+  MenuDivider,
 } from "@chakra-ui/react";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from "@chakra-ui/avatar";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-
+import { ChatState } from "../../Contex/ChatProvider";
 function SideDrawer() {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
+  const { user } = ChatState();
   return (
     <>
-      <Box        d="flex"
+      <Box
+        d="flex"
         justifyContent="space-between"
         alignItems="center"
         bg="white"
         w="100%"
         p="5px 10px 5px 10px"
-        borderWidth="5px">
+        borderWidth="5px"
+      >
         <Tooltip label="Search user to chat" hasArrow placement="bottom-end">
           <Button variant="ghost">
             <i className="fas fa-search"></i>
@@ -39,9 +45,25 @@ function SideDrawer() {
         <div>
           <Menu>
             <MenuButton p={1}>
-            <BellIcon fontSize="2xl" m={1} />
+              <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
             {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                // name={user.name}
+                // src={user.pics}
+              />
+            </MenuButton>
+
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuDivider />
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
           </Menu>
         </div>
       </Box>
